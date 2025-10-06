974b3c30e7fdee859f33a87af52b941aa753dbb5
Chinta Yanggest Br Tarigan <chintacapric836@gmail.com>
2025-10-07 05:56:41 +0700

    testing

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dashboard_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  // This is the list of screens that will be displayed for each tab.
+  static const List<Widget> _widgetOptions = <Widget>[
+    DashboardScreen(), // Our main dashboard
+    Placeholder(),   // A placeholder for the "Goals" screen
+    Placeholder(),   // A placeholder for the "Recurring" screen
+    Placeholder(),   // A placeholder for the "Learn" screen
+    Placeholder(),   // A placeholder for the "Profile" screen
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flag),
+            label: 'Goals',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sync),
+            label: 'Recurring',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Learn',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
